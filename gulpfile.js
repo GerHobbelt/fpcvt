2da93dfc61a74b51d7e7e8ffebb9fd0ae09a2217
Ger Hobbelt <ger@hobbelt.com>
2016-08-30 14:22:41 +0200

    fiddling with uglifyJS2 and gulp to see if I can get uglify to do the dead code removal, constant folding, etc. within having to hack deep inside it or code my own.

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var gulp = require('gulp');
 var jasmine = require('gulp-jasmine');
 var babel = require('gulp-babel');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 const tests_concat_filename = 'all_fpcvt_plus_tests.js';
 
@@ -17,12 +18,63 @@ gulp.task('default', ['babel'], function () {
 });
 
 gulp.task('babel', function () {
-    return gulp.src(['fpcvt*.js', 'test/spec/test.js'])
+    return gulp.src(['fpcvt.js', 'fpcvt-alt*.js', 'test/spec/test.js'])
     		.pipe(concat(tests_concat_filename))
         .pipe(babel({
             presets: ['latest'],
             babelrc: true,
             compact: false,      // don't nuke my precious formatting! ;-)
         }))
+    		.pipe(uglify({
+    			preserveComments: 'all',
+    			//output: {
+	    			mangle: false,
+	    			warnings: true,
+	    			compress: {
+	    				sequences: false,
+	    				dead_code: true,
+	    				drop_debugger: true,
+	    				conditionals: true,
+	    				comparisons: true,
+	    				unsafe_comps: true,
+	    				evaluate: true,
+	    				booleans: true,
+	    				loops: true,
+	    				unused: true,
+	    				hoist_funs: false,
+	    				hoist_vars: false,
+	    				if_return: true,
+	    				join_vars: false,
+	    				cascade: true,
+	    				collapse_vars: true,
+	    				pure_getters: false,
+	    				pure_funcs: [ 'Math.log' ],
+	    				drop_console: false,
+	    				keep_fargs: false,
+	    				keep_fnames: true,
+	    				passes: 1,
+	    				global_defs: {
+	    					DEBUG: false
+	    				},
+		    			// comments: 'all',
+	    			},
+	    			beatify: {
+	    				'indent-level': 2,
+	    				bracketize: true,
+	    				'max-line-len': 160,
+	    				semicolons: true,
+	    				preamble: '',
+	    				quote_style: 3,
+		    			// comments: function (type, value, pos, line, file, nib) {
+		    			// 	console.log('comment B', line);
+		    			// 	return true;
+		    			// },
+	    			},
+	    			comments: function (type, value, pos, line, file, nib) {
+	    				console.log('comment A', line);
+	    				return true;
+	    			},
+	    		//},
+    		}))
         .pipe(gulp.dest('test/spec/compiled'));
 }); 
